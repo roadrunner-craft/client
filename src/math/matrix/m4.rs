7a460a1c881044647e10017f74fd5bc5ba0f0dcc
corb3nik <ian@subnet192.com>
2020-05-08 20:09:39 -0400

    Add some m4 tests

--- src/math/matrix/m4.rs
+++ src/math/matrix/m4.rs
@@ -160,4 +160,48 @@ mod tests {
 
         assert_eq!(expects, super::M4::zero(), "Did not create a zero matrix");
     }
+
+    #[test]
+    fn identity_creates_identity_matrix() {
+        let expects = super::M4([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+
+        assert_eq!(expects, super::M4::identity(), "Did not create an identity matrix");
+    }
+
+    #[test]
+    fn dimensions_return_4() {
+        let expects = 4;
+        let matrix = super::M4([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]);
+
+        assert_eq!(expects, matrix.dimensions(), "Did not return dimensions of 4");
+    }
+
+    #[test]
+    fn transpose_transposes_matrix() {
+        let expects = &mut super::M4([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]);
+
+        let matrix = &mut super::M4([
+            [1.0, 5.0, 9.0, 13.0],
+            [2.0, 6.0, 10.0, 14.0],
+            [3.0, 7.0, 11.0, 15.0],
+            [4.0, 8.0, 12.0, 16.0],
+        ]);
+
+        assert_eq!(expects, matrix.transpose(), "Did not transpose matrix");
+    }
 }
