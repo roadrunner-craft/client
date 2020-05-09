6ad8cd66e7e9d5c799f3f301962ff966e0c07681
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-09 16:28:43 -0400

    generalized the  view_matrix into transform

--- src/components/transform.rs
+++ src/components/transform.rs
@@ -9,7 +9,6 @@ pub struct Transform {
 }
 
 impl Transform {
-    #[allow(dead_code)]
     pub fn new_position(x: f32, y: f32, z: f32) -> Self {
         let mut t = Self {
             position: Vector3 { x, y, z },
@@ -30,7 +29,6 @@ impl Transform {
         t
     }
 
-    #[allow(dead_code)]
     pub fn new(position: Vector3, rotation: Vector3, scale: Vector3) -> Self {
         let mut t = Self {
             position,
@@ -47,20 +45,18 @@ impl Transform {
         self.position
     }
 
-    pub fn set_position(&mut self, value: Vector3) -> &mut Self {
+    pub fn set_position(&mut self, value: Vector3) {
         self.position = value;
         self.generate_matrix();
-        self
     }
 
     pub fn get_scale(&self) -> Vector3 {
         self.scale
     }
 
-    pub fn set_scale(&mut self, value: Vector3) -> &mut Self {
+    pub fn set_scale(&mut self, value: Vector3) {
         self.scale = value;
         self.generate_matrix();
-        self
     }
 
     pub fn get_euler_angles(&self) -> Vector3 {
@@ -78,7 +74,7 @@ impl Transform {
     }
 
     fn generate_matrix(&mut self) {
-        let ((cos_x, sin_x), (cos_y, sin_y), (cos_z, sin_z)) = (
+        let ((cx, sx), (cy, sy), (cz, sz)) = (
             (
                 self.rotation.x.to_radians().cos(),
                 self.rotation.x.to_radians().sin(),
@@ -93,26 +89,45 @@ impl Transform {
             ),
         );
 
-        let m00 = cos_x * cos_y * self.scale.x;
-        let m01 = cos_x * sin_y * sin_z - sin_x * cos_z;
-        let m02 = cos_x * sin_y * cos_z + sin_x * sin_z;
-        let m10 = sin_x * cos_y;
-        let m11 = sin_x * sin_y * sin_z + cos_x * cos_z * self.scale.y;
-        let m12 = sin_x * sin_y * cos_z - cos_x * sin_z;
-        let m20 = -sin_y;
-        let m21 = cos_y * sin_z;
-        let m22 = cos_y * cos_z * self.scale.z;
+        let mut m = Matrix4::identity();
 
-        let m03 = self.position.x;
-        let m13 = self.position.y;
-        let m23 = self.position.z;
-
-        self.m = Some(Matrix4([
-            [m00, m01, m02, m03],
-            [m10, m11, m12, m13],
-            [m20, m21, m22, m23],
+        // TODO: reduce this into a single Matrix4 assignment
+        m = m * Matrix4([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, cx, sx, 0.0],
+            [0.0, -sx, cx, 0.0],
             [0.0, 0.0, 0.0, 1.0],
-        ]));
+        ]);
+
+        m = m * Matrix4([
+            [cy, 0.0, -sy, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [sy, 0.0, cy, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+
+        m = m * Matrix4([
+            [cz, sz, 0.0, 0.0],
+            [-sz, cz, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+
+        m = m * Matrix4([
+            [1.0, 0.0, 0.0, self.position.x],
+            [0.0, 1.0, 0.0, self.position.y],
+            [0.0, 0.0, 1.0, self.position.z],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+
+        m = m * Matrix4([
+            [self.scale.x, 0.0, 0.0, 0.0],
+            [0.0, self.scale.y, 0.0, 0.0],
+            [0.0, 0.0, self.scale.z, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+
+        self.m = Some(m);
     }
 }
 
