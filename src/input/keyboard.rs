32d5cddef727ecb579a5fffa6a0859b1dfd104f6
filedesless <filedesless@gmail.com>
2020-05-14 00:12:46 -0400

    added keyboard press change detection
    gives on_{press,release}-like functionality

--- src/input/keyboard.rs
+++ src/input/keyboard.rs
@@ -4,6 +4,8 @@ use std::collections::HashSet;
 #[derive(Debug, Default)]
 pub struct KeyboardHandler {
     pressed: HashSet<VirtualKeyCode>,
+    pressed_last_frame: HashSet<VirtualKeyCode>,
+    released_last_frame: HashSet<VirtualKeyCode>,
 }
 
 impl KeyboardHandler {
@@ -19,13 +21,40 @@ impl KeyboardHandler {
             // println!("{:?}", keycode);
 
             match state {
-                ElementState::Pressed => self.pressed.insert(keycode),
-                ElementState::Released => self.pressed.remove(&(keycode)),
+                ElementState::Pressed => {
+                    if !self.is_pressed(keycode) {
+                        self.pressed_last_frame.insert(keycode);
+                    }
+                    self.pressed.insert(keycode);
+                }
+                ElementState::Released => {
+                    if self.is_pressed(keycode) {
+                        self.released_last_frame.insert(keycode);
+                    }
+                    self.pressed.remove(&(keycode));
+                }
             };
         }
     }
 
+    /// indicates a currently is currently being held
     pub fn is_pressed(&self, keycode: VirtualKeyCode) -> bool {
         self.pressed.contains(&(keycode))
+    }
+
+    /// indicates a previously unpressed key was just pressed
+    pub fn just_pressed(&self, keycode: VirtualKeyCode) -> bool {
+        self.pressed_last_frame.contains(&keycode)
+    }
+
+    /// indicates a previously pressed key was just released
+    pub fn just_released(&self, keycode: VirtualKeyCode) -> bool {
+        self.released_last_frame.contains(&keycode)
+    }
+
+    /// to call at the end of each gameloop
+    pub fn clear_keyboard_state(&mut self) {
+        self.pressed_last_frame.clear();
+        self.released_last_frame.clear()
     }
 }
