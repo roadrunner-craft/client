174d5c494eee2bd889a7d90666825ab0e4035fa5
filedesless <filedesless@gmail.com>
2020-05-14 00:35:19 -0400

    getting rid of input proxy

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,8 +1,5 @@
 mod cursor;
-mod input_handler;
-pub mod keyboard;
+mod keyboard;
 
-pub use self::cursor::CursorDelta;
 pub use self::cursor::CursorHandler;
-pub use self::input_handler::InputHandler;
 pub use self::keyboard::KeyboardHandler;
