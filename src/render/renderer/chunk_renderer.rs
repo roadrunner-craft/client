ac4b2b7205ba73434deb7950fc4c944ec5b8c532
filedesless <filedesless@google.com>
2020-05-14 21:59:01 -0400

    undo mesh gen based on render distance as it's laggy

--- src/render/renderer/chunk_renderer.rs
+++ src/render/renderer/chunk_renderer.rs
@@ -168,7 +168,7 @@ impl ChunkRenderer {
         }
     }
 
-    pub fn update(&mut self, world: &World, player_position: Vector3, input: &InputHandler) {
+    pub fn update(&mut self, world: &World, input: &InputHandler) {
         if input.just_pressed(VirtualKeyCode::J) && self.render_distance > 2 {
             self.render_distance -= 1;
         }
@@ -177,19 +177,15 @@ impl ChunkRenderer {
         }
 
         // find newly generated chunks
-        let player_chunk = ChunkGridCoordinate::from_world_coordinate(player_position);
-        let render = Some(self.render_distance as usize);
         let new_chunks = world
             .chunks
             .keys()
-            .filter(|coords| player_chunk.manhattan_distance(**coords) < render)
             .filter(|coords| !self.meshes.contains_key(coords))
             .collect::<Vec<&ChunkGridCoordinate>>();
 
         // remove unloaded chunk and new chunks neighbour
         self.meshes.retain(|coords, _| {
             world.chunks.contains_key(coords)
-                && player_chunk.manhattan_distance(*coords) < render
                 && new_chunks
                     .iter()
                     .all(|other| !ChunkGridCoordinate::are_neighbours(coords, other))
@@ -197,9 +193,7 @@ impl ChunkRenderer {
 
         // generate missing geometry for loaded chunks
         for coords in world.chunks.keys() {
-            if !self.meshes.contains_key(coords)
-                && player_chunk.manhattan_distance(*coords) < render
-            {
+            if !self.meshes.contains_key(coords) {
                 let chunk_group = world.get_chunk_group(*coords);
                 self.meshes.insert(
                     *coords,
