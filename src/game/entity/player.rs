8c5301cd0d732fe2f6e3476ead2421821955af86
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-20 00:55:52 -0400

    renamed position delta to elapsed time

--- src/game/entity/player.rs
+++ src/game/entity/player.rs
@@ -7,7 +7,7 @@ pub struct Player {
     position: WorldCoordinate,
     start_position: WorldCoordinate,
     target_position: WorldCoordinate,
-    delta: f64,
+    elapsed_time: f64,
 }
 
 impl Player {
@@ -17,7 +17,7 @@ impl Player {
             position: WorldCoordinate::zero(),
             start_position: WorldCoordinate::zero(),
             target_position: WorldCoordinate::zero(),
-            delta: 0.0,
+            elapsed_time: 0.0,
         }
     }
 
@@ -28,14 +28,14 @@ impl Player {
     pub fn set_position(&mut self, position: WorldCoordinate) {
         self.start_position = self.position;
         self.target_position = position;
-        self.delta = 0.0;
+        self.elapsed_time = 0.0;
     }
 
     pub fn update(&mut self, time_delta: f64) {
-        self.delta += time_delta * 1000.0;
+        self.elapsed_time += time_delta * 1000.0;
 
-        // dirty hacks
-        let mut percent = self.delta as f32 / (2 * NETWORK_REFRESH_TIMEOUT) as f32;
+        let mut percent = self.elapsed_time as f32 / (2 * NETWORK_REFRESH_TIMEOUT) as f32;
+
         if percent > 1.0 {
             percent = 1.0;
         }
