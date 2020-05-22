a39a2a2685c0e0863f9ab71cb11a6afd117720b5
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-22 01:02:06 -0400

    player lerp cleanup

--- src/game/entity/player.rs
+++ src/game/entity/player.rs
@@ -1,13 +1,14 @@
 use crate::NETWORK_REFRESH_TIMEOUT;
 
 use core::world::WorldCoordinate;
+use std::time::Instant;
 
 pub struct Player {
     id: u128,
     position: WorldCoordinate,
     start_position: WorldCoordinate,
     target_position: WorldCoordinate,
-    elapsed_time: f64,
+    last_update: Instant,
 }
 
 impl Player {
@@ -17,7 +18,7 @@ impl Player {
             position: WorldCoordinate::zero(),
             start_position: WorldCoordinate::zero(),
             target_position: WorldCoordinate::zero(),
-            elapsed_time: 0.0,
+            last_update: Instant::now(),
         }
     }
 
@@ -28,17 +29,13 @@ impl Player {
     pub fn set_position(&mut self, position: WorldCoordinate) {
         self.start_position = self.position;
         self.target_position = position;
-        self.elapsed_time = 0.0;
+        self.last_update = Instant::now();
     }
 
-    pub fn update(&mut self, time_delta: f64) {
-        self.elapsed_time += time_delta * 1000.0;
-
-        let mut percent = self.elapsed_time as f32 / (2 * NETWORK_REFRESH_TIMEOUT) as f32;
-
-        if percent > 1.0 {
-            percent = 1.0;
-        }
+    pub fn update(&mut self) {
+        let percent = (self.last_update.elapsed().as_secs_f32() * 1000.0
+            / (2 * NETWORK_REFRESH_TIMEOUT) as f32)
+            .min(1.0);
 
         self.position =
             self.start_position + (self.target_position - self.start_position) * percent;
