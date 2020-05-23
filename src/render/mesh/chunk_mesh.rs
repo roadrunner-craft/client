f76a74aa7177d31fcd14ad97deb448ed01d4bf4b
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-23 18:46:35 -0400

    transparency support

--- src/render/mesh/chunk_mesh.rs
+++ src/render/mesh/chunk_mesh.rs
@@ -7,6 +7,162 @@ use core::chunk::{CHUNK_DEPTH, CHUNK_HEIGHT, CHUNK_WIDTH};
 use gl::types::GLuint;
 use math::vector::Vector3;
 
+pub struct ChunkMeshCollection {
+    solid: ChunkMesh,
+    flora: ChunkMesh,
+}
+
+impl ChunkMeshCollection {
+    pub fn generate(chunks: &ChunkGroup, block_database: &BlockRegistry) -> Self {
+        let mut solid_mesh = ChunkMesh::default();
+        let mut flora_mesh = ChunkMesh::default();
+
+        for x in 0..CHUNK_WIDTH {
+            for y in 0..CHUNK_HEIGHT {
+                for z in 0..CHUNK_DEPTH {
+                    let x = x as i8;
+                    let y = y as i16;
+                    let z = z as i8;
+
+                    let block = chunks.get_block(x, y, z).unwrap();
+
+                    if block.id == 0 {
+                        continue;
+                    }
+
+                    if let Some(properties) = block_database.properties(block.id) {
+                        let position = Vector3 {
+                            x: x as f32,
+                            y: y as f32,
+                            z: z as f32,
+                        };
+
+                        if properties.flora {
+                            // add cross faces
+                            continue;
+                        }
+
+                        let mesh = &mut solid_mesh;
+
+                        let mut block = chunks.get_block(x, y, z - 1);
+                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
+                            mesh.add_face(FRONT_FACE, position, properties.texture.front);
+                        }
+
+                        block = chunks.get_block(x, y, z + 1);
+                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
+                            mesh.add_face(BACK_FACE, position, properties.texture.back);
+                        }
+
+                        block = chunks.get_block(x - 1, y, z);
+                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
+                            mesh.add_face(LEFT_FACE, position, properties.texture.left);
+                        }
+
+                        block = chunks.get_block(x + 1, y, z);
+                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
+                            mesh.add_face(RIGHT_FACE, position, properties.texture.right);
+                        }
+
+                        block = chunks.get_block(x, y + 1, z);
+                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
+                            mesh.add_face(TOP_FACE, position, properties.texture.top);
+                        }
+
+                        block = chunks.get_block(x, y - 1, z);
+                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
+                            mesh.add_face(BOTTOM_FACE, position, properties.texture.bottom);
+                        }
+                    }
+                }
+            }
+        }
+
+        solid_mesh.generate();
+        flora_mesh.generate();
+
+        Self {
+            solid: solid_mesh,
+            flora: flora_mesh,
+        }
+    }
+}
+
+impl Drawable for ChunkMeshCollection {
+    fn draw(&self) {
+        unsafe {
+            gl::Enable(gl::DEPTH_TEST);
+            gl::Enable(gl::CULL_FACE);
+
+            self.solid.draw();
+
+            gl::Disable(gl::CULL_FACE);
+
+            self.flora.draw();
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct ChunkMesh {
+    mesh: Option<Mesh>,
+    vertices: Vec<Vector3>,
+    vertices_info: Vec<GLuint>,
+    vertex_count: GLuint,
+    indices: Vec<GLuint>,
+}
+
+impl ChunkMesh {
+    fn add_face(&mut self, face: Face, position: Vector3, texture_id: u8) {
+        for i in 0..4 {
+            self.vertices.push(face.vertices[i] + position);
+
+            let info: GLuint = (texture_id as GLuint) << 4
+                | ((face.light & 0b11) << 2) as GLuint
+                | (i & 0b11) as GLuint;
+            self.vertices_info.push(info);
+        }
+
+        self.indices.push(self.vertex_count);
+        self.indices.push(self.vertex_count + 3);
+        self.indices.push(self.vertex_count + 1);
+        self.indices.push(self.vertex_count + 1);
+        self.indices.push(self.vertex_count + 3);
+        self.indices.push(self.vertex_count + 2);
+
+        self.vertex_count += 4;
+    }
+
+    fn generate(&mut self) {
+        if !self.vertices.is_empty() {
+            self.mesh = Some(Mesh::new(&self.vertices, &self.indices));
+            self.mesh.as_mut().unwrap().add_vbo(&self.vertices_info);
+        }
+    }
+}
+
+impl Drawable for ChunkMesh {
+    fn draw(&self) {
+        if let Some(mesh) = self.mesh.as_ref() {
+            return mesh.draw();
+        }
+    }
+}
+
+impl Bindable for ChunkMesh {
+    fn bind(&self) {
+        if let Some(mesh) = self.mesh.as_ref() {
+            mesh.bind();
+        }
+    }
+
+    fn unbind(&self) {
+        if let Some(mesh) = self.mesh.as_ref() {
+            mesh.unbind();
+        }
+    }
+}
+
 struct Face {
     vertices: [Vector3; 4],
     light: u8,
@@ -167,120 +323,3 @@ const BOTTOM_FACE: Face = Face {
     ],
     light: 0,
 };
-
-#[derive(Default)]
-pub struct ChunkMesh {
-    mesh: Option<Mesh>,
-    vertices: Vec<Vector3>,
-    vertices_info: Vec<GLuint>,
-    vertex_count: GLuint,
-    indices: Vec<GLuint>,
-}
-
-impl ChunkMesh {
-    fn add_face(&mut self, face: Face, position: Vector3, texture_id: u8) {
-        for i in 0..4 {
-            self.vertices.push(face.vertices[i] + position);
-
-            let info: GLuint = (texture_id as GLuint) << 4
-                | ((face.light & 0b11) << 2) as GLuint
-                | (i & 0b11) as GLuint;
-            self.vertices_info.push(info);
-        }
-
-        self.indices.push(self.vertex_count);
-        self.indices.push(self.vertex_count + 3);
-        self.indices.push(self.vertex_count + 1);
-        self.indices.push(self.vertex_count + 1);
-        self.indices.push(self.vertex_count + 3);
-        self.indices.push(self.vertex_count + 2);
-
-        self.vertex_count += 4;
-    }
-
-    pub fn generate(chunks: &ChunkGroup, block_database: &BlockRegistry) -> ChunkMesh {
-        let mut mesh = ChunkMesh::default();
-
-        for x in 0..CHUNK_WIDTH {
-            for y in 0..CHUNK_HEIGHT {
-                for z in 0..CHUNK_DEPTH {
-                    let x = x as i8;
-                    let y = y as i16;
-                    let z = z as i8;
-
-                    let block = chunks.get_block(x, y, z).unwrap();
-
-                    if block.id == 0 {
-                        continue;
-                    }
-
-                    if let Some(properties) = block_database.properties(block.id) {
-                        let position = Vector3 {
-                            x: x as f32,
-                            y: y as f32,
-                            z: z as f32,
-                        };
-
-                        let mut block = chunks.get_block(x, y, z - 1);
-                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
-                            mesh.add_face(FRONT_FACE, position, properties.texture.front);
-                        }
-
-                        block = chunks.get_block(x, y, z + 1);
-                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
-                            mesh.add_face(BACK_FACE, position, properties.texture.back);
-                        }
-
-                        block = chunks.get_block(x - 1, y, z);
-                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
-                            mesh.add_face(LEFT_FACE, position, properties.texture.left);
-                        }
-
-                        block = chunks.get_block(x + 1, y, z);
-                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
-                            mesh.add_face(RIGHT_FACE, position, properties.texture.right);
-                        }
-
-                        block = chunks.get_block(x, y + 1, z);
-                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
-                            mesh.add_face(TOP_FACE, position, properties.texture.top);
-                        }
-
-                        block = chunks.get_block(x, y - 1, z);
-                        if block.is_none() || !block_database.is_opaque(block.unwrap().id) {
-                            mesh.add_face(BOTTOM_FACE, position, properties.texture.bottom);
-                        }
-                    }
-                }
-            }
-        }
-
-        if !mesh.vertices.is_empty() {
-            mesh.mesh = Some(Mesh::new(&mesh.vertices, &mesh.indices));
-            mesh.mesh.as_mut().unwrap().add_vbo(&mesh.vertices_info);
-        }
-        mesh
-    }
-}
-
-impl Drawable for ChunkMesh {
-    fn draw(&self) {
-        if let Some(ref mesh) = self.mesh {
-            return mesh.draw();
-        }
-    }
-}
-
-impl Bindable for ChunkMesh {
-    fn bind(&self) {
-        if let Some(ref mesh) = self.mesh {
-            mesh.bind();
-        }
-    }
-
-    fn unbind(&self) {
-        if let Some(ref mesh) = self.mesh {
-            mesh.unbind();
-        }
-    }
-}
